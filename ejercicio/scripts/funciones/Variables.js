21c84904672ca0c18aff162120fc9a435df92da6
= <=>
2020-02-11 16:19:38 -0300

    mascotas por eje ok, falta actualizar las imagenes con el mismo nombre

--- ejercicio/scripts/funciones/Variables.js
+++ ejercicio/scripts/funciones/Variables.js
@@ -29,51 +29,87 @@ export let srcImgRespuestaCorrecta, srcImgRespuestaIncorrecta, srcImgGlosa
 switch(idEjercicio.substr(2,2)) {
 	case '00':
 		srcImgRespuestaCorrecta = [
-			'../../../../imagenes_front/patos/Correct_feedback_PATO_nino.svg',
-			'../../../../imagenes_front/patos/Correct_feedback_PATO_nina.svg'
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_CorrectFeedback_Pata.svg',
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_CorrectFeedback_Pato.svg'
 		]
 		srcImgRespuestaIncorrecta = [
-			'../../../../imagenes_front/patos/Mistake_feedback_PATO_nino.svg',
-			'../../../../imagenes_front/patos/Mistake_feedback_PATO_nina.svg'
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_MistakeFeedback_Pata.svg',
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_MistakeFeedback_Pato.svg'
 		]
 		srcImgGlosa = [
-			'../../../../imagenes_front/patos/Pato_nina_glosa.svg',
-			'../../../../imagenes_front/patos/Pato_nino_glosa.svg'
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_Glosa_Pata.svg',
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_Glosa_Pato.svg'
 		]
 		break
 	case '01':
 		srcImgRespuestaCorrecta = [
-			'../../../../imagenes_front/patos/Correct_feedback_PATO_nino.svg',
-			'../../../../imagenes_front/patos/Correct_feedback_PATO_nina.svg'
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_CorrectFeedback_Pata.svg',
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_CorrectFeedback_Pato.svg'
 		]
 		srcImgRespuestaIncorrecta = [
-			'../../../../imagenes_front/patos/Mistake_feedback_PATO_nino.svg',
-			'../../../../imagenes_front/patos/Mistake_feedback_PATO_nina.svg'
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_MistakeFeedback_Pata.svg',
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_MistakeFeedback_Pato.svg'
 		]
 		srcImgGlosa = [
-			'../../../../imagenes_front/patos/Pato_nina_glosa.svg',
-			'../../../../imagenes_front/patos/Pato_nino_glosa.svg'
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_Glosa_Pata.svg',
+			'../../../../imagenes_front/Mascotas_Eje1/Eje1_Glosa_Pato.svg'
 		]
 		break
 	case '02':
 		srcImgRespuestaCorrecta = [
-			'../../../../imagenes_front/pumas/Correct_feedback_PUMA_nino.svg',
-			'../../../../imagenes_front/pumas/Correct_feedback_PUMA_nina.svg'
+			'../../../../imagenes_front/Mascotas_Eje2/Eje2_CorrectFeedback_Alce.svg',
+			'../../../../imagenes_front/Mascotas_Eje2/Eje2_CorrectFeedback_Zorro.svg'
 		]
 		srcImgRespuestaIncorrecta = [
-			'../../../../imagenes_front/pumas/Mistake_feedback_PUMA_nino.svg',
-			'../../../../imagenes_front/pumas/Mistake_feedback_PUMA_nina.svg'
+			'../../../../imagenes_front/Mascotas_Eje2/Eje2_MistakeFeedback_Alce.svg',
+			'../../../../imagenes_front/Mascotas_Eje2/Eje2_MistakeFeedback_Zorro.svg'
 		]
 		srcImgGlosa = [
-			'../../../../imagenes_front/pumas/Puma_nina_glosa.svg',
-			'../../../../imagenes_front/pumas/Puma_nino_glosa.svg'
+			'../../../../imagenes_front/Mascotas_Eje2/Eje2_Glosa_Zorro.svg',
+			'../../../../imagenes_front/Mascotas_Eje2/Eje2_Glosa_Alce.svg'
 		]
 		break
 	case '03':
+		srcImgRespuestaCorrecta = [
+			'../../../../imagenes_front/Mascotas_Eje3/Eje3_CorrectFeedback_Mandril.svg',
+			'../../../../imagenes_front/Mascotas_Eje3/Eje3_CorrectFeedback_Tigre.svg'
+		]
+		srcImgRespuestaIncorrecta = [
+			'../../../../imagenes_front/Mascotas_Eje3/Eje3_MistakeFeedback_Mandril.svg',
+			'../../../../imagenes_front/Mascotas_Eje3/Eje3_MistakeFeedback_Tigre.svg'
+		]
+		srcImgGlosa = [
+			'../../../../imagenes_front/Mascotas_Eje3/Eje3_Glosa_Mandril.svg',
+			'../../../../imagenes_front/Mascotas_Eje3/Eje3_Glosa_Tigre.svg'
+		]
 		break
 	case '04':
+		srcImgRespuestaCorrecta = [
+			'../../../../imagenes_front/Mascotas_Eje4/Eje4_CorrectFeedback_OsoPanda.svg',
+			'../../../../imagenes_front/Mascotas_Eje4/Eje4_CorrectFeedback_PandaRojo.svg'
+		]
+		srcImgRespuestaIncorrecta = [
+			'../../../../imagenes_front/Mascotas_Eje4/Eje4_MistakeFeedback_OsoPanda.svg',
+			'../../../../imagenes_front/Mascotas_Eje4/Eje4_MistakeFeedback_PandaRojo.svg'
+		]
+		srcImgGlosa = [
+			'../../../../imagenes_front/Mascotas_Eje4/Eje4_Glosa_OsoPanda.svg',
+			'../../../../imagenes_front/Mascotas_Eje4/Eje4_Glosa_PandaRojo.svg'
+		]
 		break
 	case '05':
+		srcImgRespuestaCorrecta = [
+			`../../../../imagenes_front/Mascotas_Eje5/Eje5_CorrectFeedback_Camaleon${Math.floor(Math.random()*5)+1}.svg`,
+			'../../../../imagenes_front/Mascotas_Eje5/Eje5_CorrectFeedback_Tortuga.svg'
+		]
+		srcImgRespuestaIncorrecta = [
+			`../../../../imagenes_front/Mascotas_Eje5/Eje5_MistakeFeedback_Camaleon${Math.floor(Math.random()*5)+1}.svg`,
+			'../../../../imagenes_front/Mascotas_Eje5/Eje5_MistakeFeedback_Tortuga.svg'
+		]
+		srcImgGlosa = [
+			`../../../../imagenes_front/Mascotas_Eje5/Eje5_Glosa_Camaleon${Math.floor(Math.random()*5)+1}.svg`,
+			'../../../../imagenes_front/Mascotas_Eje5/Eje5_Glosa_Tortuga.svg'
+		]
 		break
 }
 
