28c8e47e0a30adeece9a09419d3602ff56630b00
Pablo Henriquez <user@iMacLate2015.home>
2020-08-04 15:07:08 -0400

    posibilidad para probar ejercicios de segundo medio

--- bundle/webpack.config.js
+++ bundle/webpack.config.js
@@ -4,14 +4,23 @@ const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = env => {
     console.log('NODE_ENV: ', env.NODE_ENV)
+    let output = {
+        path: path.resolve(__dirname, '..', 'public', 'EJERCICIOS', 'Nivel-11', 'Eje-00', 'OA-00', 'IE-00', '110000000000000'),
+        filename: 'bundle.js'
+    }
+    if(env.NODE_ENV === 'production') {
+        console.log('nivelFolder: ', env.nivelFolder)
+        console.log('ejercicioFolder: ', env.ejercicioFolder.toString())
+        output = {
+            path: path.resolve(__dirname, '..', 'public', 'EJERCICIOS', env.nivelFolder, 'Eje-00', 'OA-00', 'IE-00', env.ejercicioFolder.toString()),
+            filename: 'bundle.js'
+        }
+    }
     return {
         mode: env.NODE_ENV,
-        devtool: env.NODE_ENV === 'develompent' ? 'source-ma' : 'inline-source-map',
+        devtool: env.NODE_ENV === 'develompent' ? 'source-map' : 'inline-source-map',
         entry: './index.js',
-        output: {
-            path: path.resolve(__dirname, '..', 'public', 'EJERCICIOS', 'Nivel-11', 'Eje-00', 'OA-00', 'IE-00', '110000000000000'),
-            filename: 'bundle.js'
-        },
+        output,
         module: {
             rules: [{
                 loader: 'babel-loader',
